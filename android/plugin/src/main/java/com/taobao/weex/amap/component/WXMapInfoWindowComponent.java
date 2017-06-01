38c3901353975c93a6f708846cc45ce20a805cf1
moxun.ljf <moxun.ljf@alibaba-inc.com>
2017-06-01 17:20:04 +0800

    hack

--- android/plugin/src/main/java/com/taobao/weex/amap/component/WXMapInfoWindowComponent.java
+++ android/plugin/src/main/java/com/taobao/weex/amap/component/WXMapInfoWindowComponent.java
@@ -133,18 +133,18 @@ public class WXMapInfoWindowComponent extends AbstractMapWidgetContainer<Marker>
           wxMapViewComponent.getCachedInfoWindow().put(marker.getId(), WXMapInfoWindowComponent.this);
           marker.setClickable(false);
           setMarkerPosition(marker, position);
-          marker.showInfoWindow();
-          marker.hideInfoWindow();
-          getHostView().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-              if (open) {
-                marker.showInfoWindow();
-              } else {
-                marker.hideInfoWindow();
-              }
-            }
-          }, 16);
+//          marker.showInfoWindow();
+//          marker.hideInfoWindow();
+//          getHostView().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//              if (open) {
+//                marker.showInfoWindow();
+//              } else {
+//                marker.hideInfoWindow();
+//              }
+//            }
+//          }, 16);
         }
       });
     }
